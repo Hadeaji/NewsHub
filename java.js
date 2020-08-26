3c853877d7fc17638e4629cc1208a65ea1d82620
Hadi Aji <hadeaji@gmail.com>
2020-08-26 18:48:21 +0300

    loop

--- java.js
+++ java.js
@@ -1,2 +1,6 @@
-var name = prompt('Whats is your name mate?')
+var name = prompt('Whats Is First Your Name Mate?')
+
+while (!(isNaN(name)) || name =='' || name =='null' )
+ name = prompt(`Please Write Your Name`)
+
 document.write(`${name}`)
---
+++ java3.js
@@ -0,0 +1,12 @@
+
+
+
+var time = prompt('How Much time You Watch News Weekly?')
+while ((isNaN(time)) || time =='' || time =='null' )
+time = prompt(`Please Write A Number`)
+
+
+
+
+
+document.write(`<p id=bb>${time} Hour Good For You</p>`)
